b2614ddb45ccd1b9785c959bb3919e6293570fc4
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2023-05-10 14:26:15 -0300

    Agrego funcion para listar hamburguesas

--- app/Http/Controllers/HamburguesaController.php
+++ app/Http/Controllers/HamburguesaController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Hamburguesa;
 class HamburguesaController extends Controller
 {
-    //
+    public function Listar(Request $request){
+        return Hamburguesa::all();
+    }
 }
